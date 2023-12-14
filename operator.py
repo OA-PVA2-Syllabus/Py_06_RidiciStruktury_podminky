1d6b7b7f9b0ac38acbf85dc608f45948b93c298d
Adam Fišer <adam.fiser@wanex.cz>
2023-12-14 08:21:14 +0100

    operator

---
+++ operator.py
@@ -0,0 +1,12 @@
+# Logické operátory porovnávají příkazy a vracejí výsledky v logických hodnotách.
+# Logický operátor AND vrací hodnotu True, pokud jsou výrazy na obou stranách operátoru AND pravdivé.
+# Logický operátor OR vrací hodnotu True, pokud je alespoň jeden výraz na obou stranách operátoru nebo True.
+# Logický operátor not invertuje logický výraz, kterému předchází.
+
+name = "Petr"
+age = 17
+
+print(name == "Petr" or name == "Petra")    # Zkontroluje, zda se jméno rovná "Petr" nebo "Petra".
+
+# TODO: Napište výraz, který se vyhodnotí jako True, pokud se jméno rovná "Petr" a je mu 16 nebo více let.
+print(???)
